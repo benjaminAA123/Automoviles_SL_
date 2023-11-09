ce576fdc63f458cc48882e919c8f62546e68f6eb
benjamin <benjamin.alaama@educa.jcyl.es>
2023-11-09 19:32:14 +0100

    creado el MAIN y insertado captura del iml de lucidchart

---
+++ pull_request_template.md
@@ -0,0 +1,34 @@
+## Description de la tarea
+
+Se piden crear un diagrama de clases(lucidchart), 
+aplicacion en java
+crear un main
+y introducir una pull request
+
+## ¿Cómo se ha implementado?
+
+- Creando 4 clases en lucidchart (vehiculo, cliente, altaVehiculo y venta)
+- en intellij se creo las clases y el main  junto a generate guetter and setter
+- main llamando a todas clases creadas junto a un array list con (cliente y vehiculo)
+- descargado e importado la pull request al intellij a traves de una carpeta 
+- se creado un repositorio en gihub  y se a subido todo del intellij al git
+
+## Keywords
+
+-   pull request
+- clases
+- main
+- diagrama de clases(uml)
+- debug
+
+## Screenshots or Video
+
+![pull_r.png](..%2F..%2FDocuments%2Fpull_r.png)
+
+
+## Etapas del ciclo del software
+Se a hecho una  PLANIFICACIÓN con las clases relacionandolas y hubicando donde va cada cosa 
+Se a ANALISO y se hizo una cardinalidad con flechas con cada clase
+Se DISEÑO un diagrama de clases y se implemento en intellij
+Se CODIFICO de un planteamiento a java 
+Se hicieron pruebas a traves de un debug en la clase Main y se subio una captura como prueba
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ArrayList<Vehiculo>vehiculos= new ArrayList<>();
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.setId(65465455);
+        vehiculo.setMatricula("YWFWF54S");
+        vehiculo.setMarca("BMW");
+        vehiculo.setModelo("I3");
+        vehiculo.setColor("Azul");
+        vehiculo.setTipoCombustible("electrico");
+        vehiculo.setMotor("250 NM");
+        vehiculo.setCaballos("170 CV");
+
+
+
+
+        ArrayList<Cliente>clientes = new ArrayList<>();
+        Cliente cliente = new Cliente();
+        cliente.setDni("R32323423P");
+        cliente.setNombre("Martin Diego");
+        cliente.setApellidos("Herrrera Mendez");
+        cliente.setDireccion("Avila");
+
+
+
+        clientes.add(cliente);
+        vehiculos.add(vehiculo);
+        Venta venta = new Venta();
+        venta.setId(65465455);
+        venta.setFechaVenta("19/05/23");
+        venta.setUsuarioComprador(clientes);
+        venta.setVehiculoComprado(vehiculos);
+        venta.setFormaPago("Metalico");
+        venta.setPrecioVenta(" 35.500 " + "€");
+
+
+
+         System.out.println( "Vehiculo(1) \n"
+                 + " codigo: " + vehiculo.getId()
+                 + "      Matricula: " + vehiculo.getMatricula()
+                 + "      Marca: " + vehiculo.getMarca()
+                 + "      Modelo: " + vehiculo.getModelo()
+                 + "      Color: " + vehiculo.getColor()
+                 + "      Tipo de combustible: " + vehiculo.getTipoCombustible()
+                 + "      Motor: " + vehiculo.getMotor()
+                 + "      Caballos: " + vehiculo.getCaballos() );
+
+
+         System.out.println( "cliente(1) \n"
+                 + "dni" + cliente.getDni()
+                 + "      Nombre del cliente: " + cliente.getNombre()
+                 + "      Apellidos del cliente: " + cliente.getApellidos()
+                 + "      Direccion del cliente: " + cliente.getDireccion());
+
+
+         System.out.println( "Venta (1) \n"
+                 +"codigo de venta: " + venta.getId()
+                 + "      fecha de venta: " + venta.getFechaVenta()
+                 + "      Cliente Comprador: " + venta.getUsuarioComprador()
+                 + "      Vehiculo Comprado: " + venta.getVehiculoComprado()
+                 + "      Forma de Pago: " + venta.getFormaPago()
+                 + "      Precio de la venta: " + venta.getPrecioVenta());
     }
+
+
+
 }
